0d7c289d00c137a56aa617df388b5a9fae157d76
ginocapon <ginocapon@gmail.com>
2026-02-12 10:49:23 +0100

    Add AI classifier functions to init file

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,9 +1,13 @@
 from .bandi_statici import scrape_bandi_statici
 from .api_rss import scrape_rss_feeds
 from .api_opendata import scrape_opendata
+from .ai_classifier import enrich_bando, is_bando, analyze_bando
 
 __all__ = [
     'scrape_bandi_statici',
     'scrape_rss_feeds',
-    'scrape_opendata'
+    'scrape_opendata',
+    'enrich_bando',
+    'is_bando',
+    'analyze_bando'
 ]
