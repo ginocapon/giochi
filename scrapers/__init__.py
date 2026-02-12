496bc2474c0dc39d8a7623c48f505bd106b82ec9
ginocapon <ginocapon@gmail.com>
2026-02-12 09:56:39 +0100

    Initialize scrapers module with imports and exports

---
+++ scrapers/__init__.py
@@ -0,0 +1,9 @@
+from .bandi_statici import scrape_bandi_statici
+from .api_rss import scrape_rss_feeds
+from .api_opendata import scrape_opendata
+
+__all__ = [
+    'scrape_bandi_statici',
+    'scrape_rss_feeds',
+    'scrape_opendata'
+]
